974b4624186b91e75b4fec2d9e1b2132eb837f90
Danny <danny@Dannys-MacBook-Pro.local>
2018-12-26 20:12:43 -0500

    work

--- client/src/config.js
+++
@@ -1,6 +0,0 @@
-let imgix =  {
-  baseUrl: 'https://stella.imgix.net/uploads',
-  auth: 'PX8DSHBPWqTlCwe57AZp0lT0N4xMgtu4'
-}
-
-module.exports = imgix;
--- server/env.sh
+++
@@ -1,3 +0,0 @@
-export STREAM_APP_ID=38490
-export STREAM_KEY=mudfge4hyvhm
-export STREAM_SECRET=eqjfxdm8fue25c35vd5k4b8xbums5dy2kpg38ct9yu4vbqh92dzpv5sufuuww83s
